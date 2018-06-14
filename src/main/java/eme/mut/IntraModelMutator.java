86ecc2c1952e7d3613c80709b948ab7120474641
Jose María Moyano <i02momuj@uco.es>
2018-06-14 13:13:12 +0200

    Added LICENSE (GPLv3) and header of license in all eme classes

--- src/main/java/eme/mut/IntraModelMutator.java
+++ src/main/java/eme/mut/IntraModelMutator.java
@@ -1,3 +1,14 @@
+/*
+ * This file is part of EME algorithm.
+ *
+ * (c)  Jose Maria Moyano Murillo <jmoyano@uco.es>
+ *      Eva Lucrecia Gibaja Galindo <egibaja@uco.es>
+ *      Sebastian Ventura Soto <sventura@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 package eme.mut;
 
 import net.sf.jclec.binarray.BinArrayIndividual;
